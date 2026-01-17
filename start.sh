2321f2e8ad3200e0160875059cb45e6de46f242a
HUKAM SINGH <hukamsinghnbh@gmail.com>
2026-01-17 17:04:29 +0530

    Fix Railway Java runtime using Nixpacks

--- start.sh
+++ start.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 echo "Jai Shree Ram - Starting NaukriSphere on Railway"
-
-# Railway automatically provides PORT
 echo "Using PORT=$PORT"
 
-# Run WAR using embedded Tomcat (webapp-runner)
+java -version
+
 java -jar webapp-runner.jar --port $PORT naukari-sphere-portal-app.war
